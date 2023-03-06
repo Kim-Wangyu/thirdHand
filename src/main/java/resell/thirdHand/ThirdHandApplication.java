5afe28390fe9db3d9ec73fa3c8904f118ba1676e
Kim-Wangyu <wang90205@gmail.com>
2023-03-06 21:27:41 +0800

    first commit

---
+++ src/main/java/resell/thirdHand/ThirdHandApplication.java
@@ -0,0 +1,13 @@
+package resell.thirdHand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdHandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/resell/thirdHand/ThirdHandApplicationTests.java
@@ -0,0 +1,13 @@
+package resell.thirdHand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdHandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
